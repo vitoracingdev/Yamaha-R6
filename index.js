ecd8cc1030e62fd5dc746f142651f70f816976c4
vitor souza <vitor.engineer2811@outlook.com>
2024-06-23 20:03:12 -0300

    carcteristicas commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let name = ""
+let idade = 0
+let altura = 0
+let peso = 0
+
+
+name = prompt("vitor dos santos souza")
+idade = parseint(prompt("24: "))
+altura = parseFloat(prompt("1,74: "))
+peso = parseInt(prompt("56: "))
+
+
+let Anonasc = 0
+Anonasc - 2023 - 24 
+
+
+let imc = 0 
+imc = peso / (altura*altura)
+
+
+console.log ("ola" + name + ",voce tem " + idade + "anos, nasceu em" + Anonasc + ", tem " + altura + " de altura, pesa " + peso + "kg e seu IMC é " + imc + "kg/m")
